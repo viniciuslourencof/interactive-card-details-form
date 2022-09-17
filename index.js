b0fb423a28101a1b052fd186bdef2685d12238d6
Vinicius Lourenço Ferreira <lourencovinicius00@gmail.com>
2022-09-17 14:00:06 -0300

    Desenvolvimento do site desktop

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 
+function mascara(o,f){
+  v_obj=o
+  v_fun=f
+  setTimeout("execmascara()",1)
+}
+function execmascara(){
+  v_obj.value=v_fun(v_obj.value)
+}
+function mcc(v){  
+  v=v.replace(/^(\d{4})(\d)/g,"$1 $2");
+  v=v.replace(/^(\d{4})\s(\d{4})(\d)/g,"$1 $2 $3");
+  v=v.replace(/^(\d{4})\s(\d{4})\s(\d{4})(\d)/g,"$1 $2 $3 $4");
+  return v;
+}
 
 function submitForm () {
 
-    var valid = true, error = "", field = "", regra;         
+    var formValid = true, valid = true, error = "", field = "", regra = /^[0-9]+$/;             
 
     function validateBlank() {
         if (field.value == '') {
+          formValid = false;
           valid = false;
           field.classList.add("error-input");
           error.innerHTML = "Não pode ser vazio\r\n";
@@ -18,11 +33,12 @@ function submitForm () {
         return valid;
     }
 
-    function validateOnlyNumbers() {
+    function validateOnlyNumbers() {           
+        
+        var value = field.value.replace(/\s/g, '');        
 
-        regra = /^[0-9]+$/;        
-
-        if (!field.value.match(regra)) {
+        if (!value.match(regra)) {
+          formValid = false;
           valid = false;
           field.classList.add("error-input");
           error.innerHTML = "Formato inválido, apenas números\r\n";     
@@ -38,6 +54,7 @@ function submitForm () {
     function validateLength(length) {            
 
       if (field.value.length < length) {
+        formValid = false;
         valid = false;
         field.classList.add("error-input");
         error.innerHTML = "Tamanho inválido\r\n";     
@@ -60,8 +77,7 @@ function submitForm () {
 
     if (validateBlank()) 
       if (validateOnlyNumbers())
-        validateLength(16);      
-    
+        validateLength(16);          
 
     field = document.getElementById("details__input-dateexp-month");
     error = document.getElementById("details__input-dateexp-month-error");
@@ -82,26 +98,27 @@ function submitForm () {
       if (validateOnlyNumbers())
         validateLength(3);      
 
-    if (valid) {
-      document.getElementById('card-details__number').innerHTML = 
-        document.getElementById('details__input-ccnumber').value.substring(0,4).concat(" ", 
-                                                                                       document.getElementById('details__input-ccnumber').value.substring(4,8),
-                                                                                       " ", 
-                                                                                       document.getElementById('details__input-ccnumber').value.substring(8,12),
-                                                                                       " ", 
-                                                                                       document.getElementById('details__input-ccnumber').value.substring(12,16));  
-
-
+    if (formValid) {
+      document.getElementById('card-details__number').innerHTML = document.getElementById('details__input-ccnumber').value;                                                                        
       document.getElementById('card-details__name').innerHTML = document.getElementById('details__input-name').value;  
-      document.getElementById('card-details__dateexp').innerHTML = document.getElementById('details__input-dateexp-month').value.concat('/',
-                                                                                                                                        document.getElementById('details__input-dateexp-year').value);         
-      document.getElementById('card-details-back__cvc').innerHTML = document.getElementById('details__input-cvc').value;        
-
-      document.getElementById('details').classList.add("invisible");
-      document.getElementById('confirm').classList.remove("invisible");
+      document.getElementById('card-details__dateexp').innerHTML = document.getElementById('details__input-dateexp-month').value.concat('/',document.getElementById('details__input-dateexp-year').value);         
+      document.getElementById('card-details-back__cvc').innerHTML = document.getElementById('details__input-cvc').value;              
       
-    }     
+      document.getElementById('details').classList.add("invisible");      
+      document.getElementById('confirm').classList.remove("invisible");                                  
+    }        
     
-    
+  }
+
+  function detailsVisible() {
+      document.getElementById('details').classList.remove("invisible");
+      document.getElementById('confirm').classList.add("invisible");
+      
+      document.getElementById('details__input-ccnumber').value = '';
+      document.getElementById('details__input-name').value = '';
+      document.getElementById('details__input-dateexp-month').value = '';
+      document.getElementById('details__input-dateexp-year').value = '';
+      document.getElementById('details__input-cvc').value = '';      
+
   }
 
